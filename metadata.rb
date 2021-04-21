b568e5352381409f5dbfa57ac3b66c942ffb1b7e
Aurelien Leblond <aurelien.leblond@sap.com>
2021-04-21 11:29:08 +0100

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'b1h'
 maintainer       'sap'
 description      'Install SAP Business One for SAP HANA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.19'
+version          '0.1.20'
 
 depends          'seven_zip'
 depends		 'zypper'
